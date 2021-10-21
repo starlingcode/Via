074cbbea25dfb15e5e9650e3dc8e6c7067198717
liquidcitymotors <liquidcitymotors@gmail.com>
2021-10-21 13:07:57 -0500

    add stub class for sync3xl functionality

---
+++ modules/inc/sync3xl.hpp
@@ -0,0 +1,15 @@
+
+#ifndef INC_SYNC3XL_HPP_
+#define INC_SYNC3XL_HPP_
+
+#include "sync3.hpp"
+
+
+class ViaSync3XL : public ViaSync3 {
+
+public:
+    ViaSync3XL(std::string binPath) : ViaSync3(binPath) {}
+
+};
+
+#endif /* INC_SYNC3XL_HPP_ */
